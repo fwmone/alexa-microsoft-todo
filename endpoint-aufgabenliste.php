79ec8275b1842e7f32f25f0ef33621bac975d641
Falk W. Müller <falk@surrounded.de>
2024-07-09 09:31:37 +0200

    Übergeordnete Endpunkte ergänzen

---
+++ endpoint-aufgabenliste.php
@@ -0,0 +1,8 @@
+<?
+
+require '_config.inc.php';
+
+$skillId = ['amzn1.ask.skill.ca1e8a4e-08d5-4e13-b6c0-e99aa6e6acc0']; // paste your skill id here
+$list = "aufgabenliste";
+
+require 'endpoint.php';
